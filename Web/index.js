6f96955769738699e72f59a610a710b8adc55ba9
Henry <henryychen1@gmail.com>
2020-08-22 18:32:36 -0400

    add stuff

--- Web/index.js
+++ Web/index.js
@@ -1,4 +1,5 @@
 function authenticate(){
+    // pushing ID to local storage
     var tokenId = document.getElementById("frm1");
     localStorage.setItem("Token Id", tokenId.elements[0].value);
 }
--- Web/mainPage.js
+++ Web/mainPage.js
@@ -1,7 +1,20 @@
 function download() {
+    // Getting items associated with ID
     var id = localStorage.getItem("Token Id");
     let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://127.0.0.1:8080/images?" + "keyword=" + id, false);
+    xhttp.open("GET", "http://127.0.0.1:8080/images?" + "tokenId=" + id, false);
     xhttp.send(id);
     var data = JSON.parse(xhttp.responseText);
+
+    // Download data
+    var fileName = 'myData.json';
+    
+    // Create a blob of the data
+    var fileToSave = new Blob([JSON.stringify(data)], {
+        type: 'application/json',
+        name: fileName
+    });
+    
+    // Save the file
+    saveAs(fileToSave, fileName);
 }
