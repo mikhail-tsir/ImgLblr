06d2e7171d8a17b34306b161fe2c8cf85db34f77
Henry <henryychen1@gmail.com>
2020-08-22 19:21:39 -0400

    add stuff

--- Web/static/index.js
+++ Web/static/index.js
@@ -1,5 +1,7 @@
 function authenticate(){
     console.log("TESTING");
+
+    // Pushing ID to local storage
     var tokenId = document.getElementById("frm1");
     localStorage.setItem("Token Id", tokenId.elements[0].value);
 }
