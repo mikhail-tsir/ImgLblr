ff43b9440f4afe0cafbea596a5a0083ce13c639e
mikhail-tsir <mtsirlin@uwaterloo.ca>
2020-08-24 22:14:32 -0400

    Restructure preview functionality

--- Web/static/mainPage.js
+++ Web/static/mainPage.js
@@ -2,46 +2,37 @@ let button = document.getElementById('downloadbtn');
 let id = localStorage.getItem("tokenId");
 button.onclick = downloadData;
 
+let previewBtn = document.getElementById('previewbtn');
+previewBtn.onclick = preview;
+
 async function downloadData() {
-    // Getting items associated with ID
-    var id = localStorage.getItem("tokenId");
     button.setAttribute("download", "imgdata-"+id+".json");
     console.log("ID:", id);
 
     button.href="http://127.0.0.1:5000/images?tokenId=" + id;
-
-    let response = await fetch('http://127.0.0.1:5000/images?tokenId=' + id);
-    let data = await response.json();
-    
-    console.log("Data:", data);
 }
 
 
-function preview(type) {
-    var id = localStorage.getItem("tokenId");
-    console.log("ID:", id);
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://127.0.0.1:5000/images?" + "tokenId=" + id, false);
-    xhttp.send(id);
-    var data = JSON.parse(xhttp.responseText);
-    console.log("Data:", data);
+async function preview(type) {
+    let response = await fetch('http://127.0.0.1:5000/images?tokenId=' + id);
+    let imgdata = await response.json();
+    console.log("Data:", imgdata);
 
      // Download data
-     var filename = 'data.json';
+     var filename = "imgdata-"+id+".json";
      console.log("File name:", filename);
  
      // Create a blob of the data
-     var data = new Blob([JSON.stringify(data)], {
+     var data = new Blob([JSON.stringify(imgdata)], {
          type: 'application/json',
          name: filename
      });
 
-    var file = new Blob([data], {type: type});
+    var file = new Blob([data]);
     if (window.navigator.msSaveOrOpenBlob) // IE10+
         window.navigator.msSaveOrOpenBlob(file, filename);
     else { // Others
-        var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
+        var a = document.createElement("a"), url = URL.createObjectURL(file);
         a.href = url;
         a.preview = filename;
         document.body.appendChild(a);
