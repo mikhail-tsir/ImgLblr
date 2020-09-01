85bb9b10d09c8757262e05c43eb73674f8a99e05
mikhail-tsir <mtsirlin@uwaterloo.ca>
2020-09-01 13:46:45 -0400

    Display captured images below camera preview

--- Mobile/labeler/src/camera.page.js
+++ Mobile/labeler/src/camera.page.js
@@ -4,12 +4,44 @@ import * as Permissions from "expo-permissions";
 import { Camera } from "expo-camera";
 
 import styles from "./styles";
+import Toolbar from "./toolbar";
+import Gallery from "./gallery";
 
 export default class CameraPage extends React.Component {
   camera = null;
 
   state = {
+    captures: [],
+    // setting flash to be turned off by default
+    flashMode: Camera.Constants.FlashMode.off,
+    capturing: null,
+    // start the back camera by default
+    cameraType: Camera.Constants.Type.back,
     hasCameraPermission: null,
+  };
+
+  setFlashMode = (flashMode) => this.setState({ flashMode });
+  setCameraType = (cameraType) => this.setState({ cameraType });
+  handleCaptureIn = () => this.setState({ capturing: true });
+
+  handleCaptureOut = () => {
+    if (this.state.capturing) this.camera.stopRecording();
+  };
+
+  handleShortCapture = async () => {
+    const photoData = await this.camera.takePictureAsync();
+    this.setState({
+      capturing: false,
+      captures: [photoData, ...this.state.captures],
+    });
+  };
+
+  handleLongCapture = async () => {
+    const videoData = await this.camera.recordAsync();
+    this.setState({
+      capturing: false,
+      captures: [videoData, ...this.state.captures],
+    });
   };
 
   async componentDidMount() {
@@ -20,7 +52,13 @@ export default class CameraPage extends React.Component {
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const {
+      hasCameraPermission,
+      flashMode,
+      cameraType,
+      capturing,
+      captures,
+    } = this.state;
 
     if (hasCameraPermission === null) {
       return <View />;
@@ -29,18 +67,29 @@ export default class CameraPage extends React.Component {
     }
 
     return (
-      <SafeAreaView
-        style={{
-          paddingTop: Platform.OS === "android" ? 25 : 0,
-          flex: 1,
-        }}
-      >
-        <Camera
-          style={styles.preview}
-          ref={(camera) => (this.camera = camera)}
-          ratio="1:1"
-        />
-      </SafeAreaView>
+      <React.Fragment>
+        <SafeAreaView style={styles.previewContainer}>
+          <Camera
+            type={cameraType}
+            flashMode={flashMode}
+            style={styles.preview}
+            ref={async (camera) => (this.camera = camera)}
+            ratio="1:1"
+          />
+          {captures.length > 0 && <Gallery captures={captures} />}
+          <Toolbar
+            capturing={capturing}
+            flashMode={flashMode}
+            cameraType={cameraType}
+            setFlashMode={this.setFlashMode}
+            setCameraType={this.setCameraType}
+            onCaptureIn={this.handleCaptureIn}
+            onCaptureOut={this.handleCaptureOut}
+            onLongCapture={this.handleLongCapture}
+            onShortCapture={this.handleShortCapture}
+          />
+        </SafeAreaView>
+      </React.Fragment>
     );
   }
 }
--- Mobile/labeler/src/styles.js
+++ Mobile/labeler/src/styles.js
@@ -1,16 +1,64 @@
-import { StyleSheet, Dimensions, StatusBar } from "react-native";
+import { StyleSheet, Dimensions, StatusBar, Platform } from "react-native";
 
 const { width: winWidth, height: winHeight } = Dimensions.get("window");
 
 export default StyleSheet.create({
+  previewContainer: {
+    flex: 1,
+    backgroundColor: "black",
+    //justifyContent: "center",
+  },
   preview: {
     height: winWidth,
     width: winWidth,
-    position: "absolute",
+    //position: "absolute",
     left: 0,
-    top: StatusBar.currentHeight,
+    //top: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    top: "15%",
     right: 0,
     bottom: 0,
-    //paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "dodgerblue",
+  },
+  alignCenter: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomToolbar: {
+    width: winWidth,
+    position: "absolute",
+    height: 100,
+    bottom: 0,
+  },
+  captureBtn: {
+    width: 60,
+    height: 60,
+    borderWidth: 2,
+    borderRadius: 60,
+    borderColor: "#FFFFFF",
+  },
+  captureBtnActive: {
+    width: 80,
+    height: 80,
+  },
+  captureBtnInternal: {
+    width: 76,
+    height: 76,
+    borderWidth: 2,
+    borderRadius: 76,
+    backgroundColor: "red",
+    borderColor: "transparent",
+  },
+  galleryContainer: {
+    bottom: 100,
+  },
+  galleryImageContainer: {
+    width: 75,
+    height: 75,
+    marginRight: 5,
+  },
+  galleryImage: {
+    width: 75,
+    height: 75,
   },
 });
