b3d648ea556e639d3edc80d2cd0d45c89ee3b4c6
mikhail-tsir <mtsirlin@uwaterloo.ca>
2020-09-13 14:22:15 -0400

    create storage files when app loads

--- Mobile/labeler/App.js
+++ Mobile/labeler/App.js
@@ -1,15 +1,49 @@
 import React from "react";
 import { NavigationContainer, StackActions } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as FileSystem from "expo-file-system";
 
 import CameraPage from "./src/camera.page";
 import LabelScreen from "./src/labelscreen";
+import { dirLocation, queueLocation, savedLocation } from "./src/constants.js";
 
 const Stack = createStackNavigator();
 
 export default class App extends React.Component {
+  fileError = null;
+
+  async componentDidMount() {
+    try {
+      let dir = await FileSystem.getInfoAsync(dirLocation);
+
+      if (!dir.exists) {
+        console.log("Making directory Labeler");
+        let create = await FileSystem.makeDirectoryAsync(dirLocation);
+        if (!create) {
+          this.fileError = "Could not create directory";
+          return;
+        }
+
+        let info = await FileSystem.getInfoAsync(queueLocation);
+        if (!info.exists) {
+          FileSystem.makeDirectoryAsync(queueLocation);
+        }
+
+        info = await FileSystem.getInfoAsync(savedLocation);
+        if (!info.exists) {
+          FileSystem.makeDirectoryAsync(savedLocation);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      this.fileError = err.message;
+    }
+  }
+
   render() {
-    return (
+    return this.fileError ? (
+      <Text>{this.fileError}</Text>
+    ) : (
       <NavigationContainer>
         <Stack.Navigator headerMode={false}>
           <Stack.Screen name="cameraPage" component={CameraPage} />
---
+++ Mobile/labeler/src/constants.js
@@ -0,0 +1,5 @@
+import * as FileSystem from "expo-file-system";
+
+export const dirLocation = FileSystem.documentDirectory + "labeler/";
+export const queueLocation = dirLocation + "queue/";
+export const savedLocation = dirLocation + "saved/";
