4a5d4a5a83598096e6cb1eb8c485b8a6aab0d7c2
mikhail-tsir <mtsirlin@uwaterloo.ca>
2020-09-13 13:15:55 -0400

    Handle camera roll permission denied

--- Mobile/labeler/src/labelscreen.js
+++ Mobile/labeler/src/labelscreen.js
@@ -9,27 +9,65 @@ import {
   Platform,
   StatusBar,
 } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
 
-//import styles from "./styles";
 import Toolbar, { BackHeader } from "./labeler.toolbar";
-import Config from "./config/config";
 
-const buttonSize = Config.BUTTON_SIZE;
 const { width: winWidth, height: winHeight } = Dimensions.get("window");
 
 export default class LabelScreen extends React.Component {
+  state = { savedPhotos: null, hasCameraRollPermission: null };
   navigation = this.props.navigation;
-
   handleGoBack = this.navigation.goBack;
+
   handleCancel = () => console.log("Not actually cancelling... yet");
   handleUpload = () => console.log("Not actually uploading... yet");
-  handleSave = () => console.log("Not actually saving... yet");
+  handleSave = async () => {
+    let { hasCameraRollPermission } = this.state;
+    if (!hasCameraRollPermission) {
+      alert(
+        "Permission Denied",
+        "Camera Roll permission has been denied. Please allow access to camera roll."
+      );
+      const camroll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      let perm = camroll.status === "granted";
+      this.setState({ hasCameraRollPermission: perm });
+    } else {
+      console.log("Not saving... yet");
+    }
+  };
 
-  componentDidMount() {
+  async componentDidMount() {
     StatusBar.setHidden(true);
+    const camroll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    let hasCameraRollPermission = camroll.status === "granted";
+    this.setState({ hasCameraRollPermission });
+
+    if (hasCameraRollPermission) {
+      try {
+        const result = await this.getPhotosAsync();
+        console.log(this.state.savedPhotos);
+      } catch (err) {
+        throw Error(400);
+      }
+    }
+  }
+
+  async getPhotosAsync() {
+    let photos = await MediaLibrary.getAssetsAsync({ first: 2 });
+    this.setState({ savedPhotos: photos.assets });
   }
 
   render() {
+    const { hasCameraRollPermission } = this.state;
+    if (!hasCameraRollPermission) {
+      return <Text>Camera Roll permission has been denied</Text>;
+    }
+
+    console.log("State: " + this.state);
+    let { photos } = this.state;
+    console.log(photos);
     const { navigation, route } = this.props;
     const imgLocation = route.params.location;
 
