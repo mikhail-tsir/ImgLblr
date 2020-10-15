7ab6b3005bb5c2f197724ba18e29422e6eb032dd
mikhail-tsir <mtsirlin@uwaterloo.ca>
2020-10-15 14:57:27 -0400

    Add single image upload functionality

--- Mobile/labeler/src/labelscreen.js
+++ Mobile/labeler/src/labelscreen.js
@@ -18,12 +18,45 @@ import { queueLocation } from "./constants";
 
 const { width: winWidth, height: winHeight } = Dimensions.get("window");
 
+function getFileName(path) {
+  let temp = path.split("/");
+  return temp[temp.length - 1];
+}
+
+function queueToSavedName(queuePath) {
+  return savedLocation + getFileName(queuePath);
+}
+
+const getBlob = async (fileUri) => {
+  const res = await fetch(fileUri);
+  const imgBody = await res.blob();
+  return imgBody;
+};
+
+const uploadImage = async (url, fields, imgPath) => {
+  const form = new FormData();
+  Object.keys(fields).forEach((key) => {
+    form.append(key, fields[key]);
+  });
+  const imgBody = await getBlob(imgPath);
+  form.append("file", imgBody);
+
+  return await fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/xml",
+      "Content-Type": "multipart/form-data",
+    },
+    body: form,
+  });
+};
+
 export default class LabelScreen extends React.Component {
   state = { savedPhotos: null, hasCameraRollPermission: null };
   navigation = this.props.navigation;
   handleGoBack = this.navigation.goBack;
+  location = this.props.route.params.location;
 
-  //handleCancel = () => console.log("Not actually cancelling... yet");
   handleCancel = async () => {
     let location = this.props.route.params.location;
     await FileSystem.deleteAsync(location);
@@ -34,12 +67,36 @@ export default class LabelScreen extends React.Component {
       return;
     }
 
+    // TODO: show next image, not first image
     this.navigation.navigate("LabelScreen", {
       location: queueLocation + queueImgs[0],
     });
   };
 
-  handleUpload = () => console.log("Not actually uploading... yet");
+  handleUpload = async () => {
+    console.log("hello");
+    let response;
+    try {
+      response = await fetch(
+        "http://192.168.0.30:5000/generateurl?name=" +
+          getFileName(this.location)
+      );
+    } catch (err) {
+      alert(err);
+      return;
+    }
+
+    try {
+      const data = await response.json();
+      console.log("DATA:\n" + JSON.stringify(data));
+      let res = await uploadImage(data.url, data.fields, this.location);
+      let bruh = await res.text();
+      console.log("RESULT: " + bruh);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   handleSave = async () => {
     let { hasCameraRollPermission } = this.state;
     if (!hasCameraRollPermission) {
@@ -50,9 +107,22 @@ export default class LabelScreen extends React.Component {
       const camroll = await Permissions.askAsync(Permissions.CAMERA_ROLL);
       let perm = camroll.status === "granted";
       this.setState({ hasCameraRollPermission: perm });
-    } else {
-      console.log("Not saving... yet");
     }
+
+    //move from queue to saved
+    let location = this.location;
+
+    try {
+      await FileSystem.moveAsync({
+        from: location,
+        to: queueToSavedName(location),
+      });
+    } catch (err) {
+      console.log(error);
+      alert(error);
+      return;
+    }
+    this.handleCancel();
   };
 
   async componentDidMount() {
@@ -61,11 +131,6 @@ export default class LabelScreen extends React.Component {
     let hasCameraRollPermission = camroll.status === "granted";
     this.setState({ hasCameraRollPermission });
   }
-
-  // async getPhotosAsync() {
-  //   let photos = await MediaLibrary.getAssetsAsync({ first: 2 });
-  //   this.setState({ savedPhotos: photos.assets });
-  // }
 
   render() {
     const { hasCameraRollPermission } = this.state;
--- Web/app.py
+++ Web/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
 import json
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+s3 = boto3.resource('s3')
+bucket_name = "imglabeler"
 
 app = Flask(__name__)
 @app.route('/')
@@ -15,5 +21,17 @@ def get_imgdata():
     tokenId = request.args.get("tokenId")
     return json.dumps({'hello':'world',
             'user': tokenId})
+
+@app.route('/generateurl', methods=['GET', 'POST'])
+def create_presigned_post():
+    s3_client = boto3.client('s3')
+    object_name = request.args.get("name")
+    try:
+        response = s3_client.generate_presigned_post(bucket_name, object_name)
+    except ClientError as e:
+        print(e)
+        return None
+
+    return response
 
 app.run(port=5000)
